fc5f8e91f4d5bdd22d0d7ca7ed62c1504c07cc4b
haruk <haruu11113@gmail.com>
2023-07-21 01:37:15 +0900

    feat: update to nuxt3

--- app.vue
+++ app.vue
@@ -1,15 +1,5 @@
 <template>
-  <div>
-    <client-only>
-      <tiptap-editor />
-    </client-only>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
-<script>
-import TiptapEditor from '~/components/TiptapEditor.vue'
-export default {
-  components: {
-    TiptapEditor
-  }
-}
-</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <client-only>
+    <tiptap-editor />
+    </client-only>
+  </div>
+</template>
+
+<script lang="ts" setup>
+    import TiptapEditor from '~/components/TiptapEditor.vue'
+    definePageMeta({
+    layout: "default",
+    components: {
+        TiptapEditor
+    }
+    });
+  </script>
